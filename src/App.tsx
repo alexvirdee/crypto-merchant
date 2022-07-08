0fa4676615fc340a7cc1853a7b4e58ad44002449
Alexinder Virdee <alex.virdee08@gmail.com>
2022-07-08 16:38:17 -0400

    Mock a crypto merchant

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,123 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="text-3xl pt-2 font-bold text-center text-bitpay-blue">
-      BitPay Coding Challenge
-    </div>
+    <>
+      <div className="text-3xl pt-2 font-bold text-center text-bitpay-blue">
+        BitPay Coding Challenge
+      </div>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-center">
+                <thead className="border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Merchant
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Item
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Amount (Crypto)
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Currency
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Price/crypto (USD)
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4"
+                    >
+                      Amount (USD)
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b bg-gray-800 boder-gray-900">
+                    <td className="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                      ShirtTown
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      T-shirts
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      1.43219876
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      BTC
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $9,285.93
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $13,299.30
+                    </td>
+                  </tr>
+                  <tr className="border-b bg-gray-800 boder-gray-900">
+                    <td className="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                      ShirtTown
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      T-shirts
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      1.43219876
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      BTC
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $9,285.93
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $13,299.30
+                    </td>
+                  </tr>
+                  <tr className="border-b bg-gray-800 boder-gray-900">
+                    <td className="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                      ShirtTown
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      T-shirts
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      1.43219876
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      BTC
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $9,285.93
+                    </td>
+                    <td className="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                      $13,299.30
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      'bitpay-blue': '#2240C4',
+    extend: {
+      colors: {
+        'bitpay-blue': '#2240C4',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
