dc9ecc2d9e4379736c543c27bc76a72673665b76
Alexinder Virdee <alex.virdee08@gmail.com>
2022-07-08 16:29:25 -0400

    Remove files and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      'bitpay-blue': '#2240C4',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
